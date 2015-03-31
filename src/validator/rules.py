bdab1a105d872dc9db9600bc261f1403d07d27ab
Tomas Krizek <t@krizek.ws>
2015-03-31 19:30:26 +0200

    exception messages changed

--- src/validator/rules.py
+++ src/validator/rules.py
@@ -13,7 +13,7 @@ def check_integer(val, min=float("-inf"), max=float("inf")):
         try:
             val = int(val)      # string conversion: "3" -> 3
         except ValueError:
-            raise TypeError("Expecting 'int' type")
+            raise TypeError("Expecting type Integer")
 
     if (val < min):
         raise ValueTooSmall(min)
@@ -29,7 +29,7 @@ def check_double(val, min=float("-inf"), max=float("inf")):
         try:
             val = float(val)      # string conversion: "3.14" -> 3.14
         except ValueError:
-            raise TypeError("Expecting 'float' type")
+            raise TypeError("Expecting type Double")
 
     if (val < min):
         raise ValueTooSmall(min)
