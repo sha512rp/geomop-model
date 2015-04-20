f4852d0038a3c1392c1db6d0bc394a83584f480b
Tomas Krizek <t@krizek.ws>
2015-04-20 08:48:04 +0200

    new exceptions for array length

--- src/validator/errors.py
+++ src/validator/errors.py
@@ -34,3 +34,18 @@ class InvalidOption(ValidationError):
                 str(selection.name) + ""
 
         super(InvalidOption, self).__init__(message)
+
+
+class NotEnoughItems(ValidationError):
+    def __init__(self, expected):
+        message = "Expected at least " + str(expected) + " items"
+
+        super(ValueTooBig, self).__init__(message)
+
+
+class TooManyItems(ValidationError):
+    def __init__(self, expected):
+        message = "Expected at most " + str(expected) + " items"
+
+        super(ValueTooBig, self).__init__(message)
+
