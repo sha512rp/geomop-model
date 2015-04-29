984b03444dbd4d9135db1ba79b1fdfa26d3d8198
Tomas Krizek <t@krizek.ws>
2015-04-29 18:06:08 +0200

    updated rules (parameter: its)

--- src/validator/rules.py
+++ src/validator/rules.py
@@ -6,110 +6,69 @@ Basic rules for data validation
 """
 
 from geomopcontext.validator.errors import *
-from geomopcontext.data.selection import Selection
 
 
-class Rule:
-    OPTIONAL_PARAMS = ['name', 'full_name', 'description']
-
-    def __init__(self, data):
-        self.id = data['id']
-        self.input_type = data['input_type']
-        for param in Rule.OPTIONAL_PARAMS:       # parse optional parameters
-            try:
-                setattr(self, param, data[param])
-            except KeyError:
-                pass
-        try:        # parse extra parameters based on input_type
-            getattr(self, '_parse_%s' % self.input_type.lower())(data)
-        except AttributeError:
-            self.kwargs = {}
-
-    def _parse_integer(self, data):
-        self.kwargs = {'min': data['range'][0],\
-                     'max': data['range'][1]}
-
-    def _parse_double(self, data):
-        self.kwargs = {'min': data['range'][0],\
-                     'max': data['range'][1]}
-
-    def _parse_selection(self, data):
-        self.selection = Selection(self.name, data['values'])
-        self.kwargs = {'selection': self.selection}
-
-    def _parse_filename(self, data):
-        self.kwargs = {'file_mode': data['file_mode']}
-
-    def _parse_array(self, data):
-        self.kwargs = {'min': data['range'][0],\
-                     'max': data['range'][1]}
-        self.subtype = data['subtype']
-
-     # TODO implement _parse_record, _parse_abstract_record
-
-
-
-def check_integer(val, min=float("-inf"), max=float("inf")):
+def check_integer(val, its):
     if not isinstance(val, int):
         raise ValidationTypeError("Expecting type Integer")
 
-    if (val < min):
-        raise ValueTooSmall(min)
+    if (val < its.min):
+        raise ValueTooSmall(its.min)
 
-    if (val > max):
-        raise ValueTooBig(max)
+    if (val > its.max):
+        raise ValueTooBig(its.max)
 
     return True
 
 
-def check_double(val, min=float("-inf"), max=float("inf")):
+def check_double(val, its):
     if not isinstance(val, (int, float)):
         raise ValidationTypeError("Expecting type Double")
 
-    if (val < min):
-        raise ValueTooSmall(min)
+    if (val < its.min):
+        raise ValueTooSmall(its.min)
 
-    if (val > max):
-        raise ValueTooBig(max)
+    if (val > its.max):
+        raise ValueTooBig(its.max)
 
     return True
 
 
-def check_bool(val):
+def check_bool(val, its):
     if not isinstance(val, bool):
         raise ValidationTypeError("Expecting type Bool")
 
     return True
 
 
-def check_string(val):
+def check_string(val, its):
     if not isinstance(val, str):
         raise ValidationTypeError("Expecting type String")
 
     return True
 
 
-def check_selection(val, selection=None):
-    if (selection.has(val)):
+def check_selection(val, its):
+    if (val in its.values.keys()):
         return True
     else:
-        raise InvalidOption(selection)
+        raise InvalidOption(val, its.name)
 
 
-def check_filename(val, file_mode=None):
+def check_filename(val, its):
     """
     Placeholder for FileName validation.
     """
-    return check_string(val)
+    return check_string(val, its)
 
 
-def check_array(val, min=0, max=float("inf")):
+def check_array(val, its):
     if not isinstance(val, (list, str)):
         raise ValidationTypeError("Expecting type Array")
 
-    if len(val) < min:
-        raise NotEnoughItems(min)
-    elif len(val) > max:
-        raise TooManyItems(max)
+    if len(val) < its.min:
+        raise NotEnoughItems(its.min)
+    elif len(val) > its.max:
+        raise TooManyItems(its.max)
 
     return True
