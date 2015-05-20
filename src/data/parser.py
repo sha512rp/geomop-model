6f322afec3e2dedd045f72426e42625c8864ccad
Tomas Krizek <t@krizek.ws>
2015-05-20 13:25:24 +0200

    refactoring: parser package

---
+++ src/data/parser.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+GeomMop configuration file parsers
+
+This file contains the parsing functions for configuration files of Flow123d.
+Currently supports .con format (as specified by Flow123d manual v1.8.2).
+
+Future support for .yaml files for Flow123d v2 is planned.
+
+@author: Tomas Krizek
+"""
+
+
+import demjson
+import re
+from copy import copy
+
+from geomop.model.data.model import DataNode, RefError
+
+
+def parse_con(filename):
+    """
+    Parses a configuration file of Flow123d in .con format with given filename.
+    If the input file includes references, they are resolved and represented
+    by actual python references in the output.
+
+    Returns the root of the resulting tree DataNode structure.
+    """
+    con = open(filename).read()
+    data = _decode_con(con)
+    return _resolve_references(data)
+
+
+def _resolve_references(data):
+    """
+    Resolves references in data. Replaces REF keys with actual Python
+    references.
+    """
+    refs = _extract_references(data)
+    root = DataNode(data)
+    while len(refs) > 0:
+        length = len(refs)
+        for path, ref_path in copy(refs).items():
+            try:
+                node = root.get(path)
+                if ref_path.startswith('/'):  # absolute ref
+                    node.ref = root.get(ref_path)
+                else:  # relative ref
+                    node.ref = node.get(ref_path)
+            except LookupError:
+                continue
+            else: del refs[path]
+        if length == len(refs):
+            # no reference removed -> impossible to resolve references
+            raise RefError("Can not resolve references.")
+    return root
+
+
+def _decode_con(con):
+    """Reads .con format and returns read data in form of dicts and lists."""
+    pattern = re.compile(r"\s?=\s?")  # TODO can I replace = simply like this?
+    con = pattern.sub(':', con)
+    return demjson.decode(con)
+
+
+def _extract_references(json):
+    """
+    Crawl through the data and find all REF keys.
+
+    Returns a dictionary of their locations and where they point.
+    """
+    def crawl(data, path):
+        if isinstance(data, dict):
+            try:
+                ref_path = data['REF']
+            except KeyError:
+                pass
+            else:
+                refs[path] = ref_path
+                del data['REF']
+            # crawl for all keys
+            for key, value in data.items():
+                crawl(value, path + '/' + key)
+        elif isinstance(data, list):
+            # crawl for all records
+            for i, item in enumerate(data):
+                crawl(item, path + '/' + str(i))
+
+    refs = {}
+    crawl(json, '')
+    return refs
---
+++ src/data/test_parser.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for parser module.
+
+@author: Tomas Krizek
+"""
+
+
+import unittest
+import geomop.model.data.parser as parser
+
+
+class TestConFileHandler(unittest.TestCase):
+    def test_references(self):
+        raw = {
+            'a': {'x': 0, 'y': {'REF': '/b/y'}},
+            'b': {'REF': '/d'},
+            'c': [
+                {'REF': '/b/x'},
+                {'REF': '/a/y'}
+            ],
+            'd': {'x': 3, 'y': 5}
+        }
+        refs = parser._extract_references(raw)
+
+        self.assertEqual(refs['/a/y'], '/b/y')
+        self.assertEqual(refs['/b'], '/d')
+        self.assertEqual(refs['/c/0'], '/b/x')
+        self.assertEqual(refs['/c/1'], '/a/y')
+
+    def test_circular_reference(self):
+        raw = {
+            'a': {'REF': '/b'},
+            'b': {'REF': '/c'},
+            'c': {'REF': '/a'}
+        }
+
+        root = parser._resolve_references(raw)
+        with self.assertRaises(parser.RefError):
+            root.get('/a').value
+
+    def test_reference_error(self):
+        raw = {
+            'a': {'REF': '/b/x'},
+            'b': {'REF': '/c'},
+            'c': {'REF': '/a'}
+        }
+
+        with self.assertRaises(parser.RefError):
+            parser._resolve_references(raw)
+
+    def test_parse_json(self):
+        raw = {
+            'a': {'x': 0, 'y': {'REF': '/b/y'}},
+            'b': {'REF': '/d'},
+            'c': [
+                {'REF': '/b/x'},
+                {'REF': '/a/y'}
+            ],
+            'd': {'x': 3, 'y': 5},
+            'e': {'REF': '/c'}
+        }
+        root = parser._resolve_references(raw)
+
+        self.assertEqual(root.get('/a/x').value, 0)
+        self.assertEqual(root.get('/a/y').value, 5)
+        self.assertEqual(root.get('/b/x').value, 3)
+        self.assertEqual(root.get('/b/y').value, 5)
+        self.assertEqual(root.get('/c/0').value, 3)
+        self.assertEqual(root.get('/c/1').value, 5)
+        self.assertEqual(root.get('/d/x').value, 3)
+        self.assertEqual(root.get('/d/y').value, 5)
+        self.assertEqual(root.get('/e/0').value, 3)
+        self.assertEqual(root.get('/e/1').value, 5)
+
+        # test references
+        self.assertEqual(root.get('/a/y').value, root.get('/d/y').value)
+        self.assertEqual(root.get('/b').value, root.get('/d').value)
+        self.assertEqual(root.get('/c/0').value, root.get('/d/x').value)
+        self.assertEqual(root.get('/c/1').value, root.get('/d/y').value)
+        self.assertEqual(root.get('/e').value, root.get('/c').value)
+
+    def test_relative_reference(self):
+        raw = {
+            'a': {'x': 0, 'y': {'REF': '../../b'}},
+            'b': 2,
+        }
+        root = parser._resolve_references(raw)
+
+        self.assertEqual(root.get('/a/y')._ref, root.get('/b')._ref)
+
